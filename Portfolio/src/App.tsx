6dbf4703096577c439964e8eacae49e349b19bb7
Aadhi Saravanan <mr.aadhi2005@gmail.com>
2024-12-05 19:55:10 +0530

    first commit

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+function App() {
+
+  return (
+    <div className=" flex items-center justify-center h-screen">
+      <h1 className=" font-bold text-black text-l">AADHI's PORTFOLIO</h1>
+    </div>
+  )
+}
+
+export default App
